968b2bd1faf7f581f38065dbea1e7b8fc7aa96ca
Gürkan Çağlar <gurkan.caglar@ekinokssoftware.com>
2018-06-03 11:06:51 +0300

    sukela degisiklikler

--- Assets/Own Resources/Scripts/UIManager.cs
+++ Assets/Own Resources/Scripts/UIManager.cs
@@ -1,20 +1,48 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UIManager : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    GameObject mainCanvas;
+    GameObject settingsCanvas;
+    GameObject bookSelectionCanvas;
+    GameObject partitionSelectionCanvas;
+
+    Button settingsButton;
+    Button exitButton;
+
+
+    // Use this for initialization
+    void Start () {
+
+        mainCanvas = GameObject.Find("MainCanvas").gameObject;
+        settingsCanvas = GameObject.Find("SettingsCanvas").gameObject;
+        bookSelectionCanvas = GameObject.Find("BookSelectionCanvas").gameObject;
+        partitionSelectionCanvas = GameObject.Find("PartitionSelectionCanvas").gameObject;
+
+        Debug.Log(mainCanvas);
+        Debug.Log(settingsCanvas);
+        Debug.Log(bookSelectionCanvas);
+        Debug.Log(partitionSelectionCanvas);
+
+        settingsButton = GameObject.Find("Settings").GetComponent<Button>();
+        settingsButton.onClick.AddListener(ListenUI);
+
+        exitButton = GameObject.Find("Exit").GetComponent<Button>();
+        exitButton.onClick.AddListener(ListenUI);
+
+        ListenUI();
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
 
-	void listenUI(){
-		
+	void ListenUI(){
+
+        Debug.Log("Basildi");
 	}
 }
