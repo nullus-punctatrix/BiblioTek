08f705c0d4a0c4c57b640d106bbd3d7e58ae70f9
Gürkan Çağlar <gurkan.caglar@ekinokssoftware.com>
2018-06-05 02:40:56 +0300

    debug silmece

--- Assets/Own Resources/Scripts/eBookConverter.cs
+++ Assets/Own Resources/Scripts/eBookConverter.cs
@@ -5,18 +5,12 @@ using System.IO;
 
 public class eBookConverter : MonoBehaviour{
 
-    //	private string eBookPath;
-    //	private string convertedPath;
+    
     	private string fullText;
 
-    //private string startPath = "/BiblioTek";
-
-    //string m_Path;
-
-    // Use this for initialization
+   
 
     string path;
-    bool yarraminKirmaKolu = false;
 
     void Start()
     {
@@ -38,67 +32,10 @@ public class eBookConverter : MonoBehaviour{
         sendFullText(jsonString);
 
 
-        //Debug.Log("SAAAAAAAAAAAAASADFAFSASFASFALSIUOFGOUIAGSFPASF");
-        //path= "jar:file://" + Application.dataPath + "!/assets/";
-
-        //path += "ConvertedTextFiles/deneme.txt";
-
-        //string text = System.IO.File.ReadAllText(@path);
-
-        //Debug.Log(text.Substring(0, 50));
-
-
-        //sendFullText(text);
-        //if (Directory.Exists (startPath)) {
-
-        //} else {
-        //	Directory.CreateDirectory (startPath);
-        //}
-        //if (Debug.isDebugBuild)
-        //{
-        //    Debug.Log("This is a debug build!");
-        //    m_Path = Application.dataPath;
-
-        //    //Output the Game data path to the console
-        //    Debug.Log("Path : " + m_Path);
-        //}
-        //m_Path = Application.dataPath;
-
-        //Output the Game data path to the console
-        //Debug.Log("Path : " + m_Path);
-        //sendFullText("CHAPTER I. Down the Rabbit-Hole\nAlice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, ‘and what is the use of a book,’ thought Alice ‘without pictures or conversations?’\n\nSo she was considering in her own mind (as well as she could, for the hot day made her feel very sleepy and stupid), whether the pleasure of making a daisy-chain would be worth the trouble of getting up and picking the daisies, when suddenly a White Rabbit with pink eyes ran close by her.\n\nThere was nothing so very remarkable in that; nor did Alice think it so very much out of the way to hear the Rabbit say to itself, ‘Oh dear! Oh dear! I shall be late!’ (when she thought it over afterwards, it occurred to her that she ought to have wondered at this, but at the time it all seemed quite natural); but when the Rabbit actually took a watch out of its waistcoat-pocket, and looked at it, and then hurried on, Alice started to her feet, for it flashed across her mind that she had never before seen a rabbit with either a waistcoat-pocket, or a watch to take out of it, and burning with curiosity, she ran across the field after it, and fortunately was just in time to see it pop down a large rabbit-hole under the hedge.\n\nIn another moment down went Alice after it, never once considering how in the world she was to get out again.\n\nThe rabbit-hole went straight on like a tunnel for some way, and then dipped suddenly down, so suddenly that Alice had not a moment to think about stopping herself before she found herself falling down a very deep well.");
     }
 	
 	// Update is called once per frame
 	void Update () {
-
-        if (!yarraminKirmaKolu)
-        {
-
-            //Debug.Log("SAAAAAAAAAAAAASADFAFSASFASFALSIUOFGOUIAGSFPASF");
-
-            //yarraminKirmaKolu = true;
-
-            //string path2 = "jar:file:/" + Application.dataPath + "!/assets/ConvertedTextFiles/deneme.txt";
-
-            //string path3 = "Assets/StreamingAssets/ConvertedTextFiles/deneme.txt";
-
-            //Debug.Log("BURDAPATLIYOSANANANISIKIYIM");
-
-            //StreamReader reader = new StreamReader(path2);
-
-            //Debug.Log("YARRAQUE");
-
-            //sendFullText(reader.ReadToEnd());
-
-            //Debug.Log("DELIGINEZUKUSUYDU");
-
-            //reader.Close();
-
-            //Debug.Log("LOL IQ");
-
-            
-        }
         
     }
 
